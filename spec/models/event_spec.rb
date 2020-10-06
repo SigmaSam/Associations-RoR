042015f0079370a8951d49bc4493b7ce524e192b
Samuel <samuelalmaoherrera@gmail.com>
2020-10-06 12:36:50 -0300

    add extra space.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,7 +2,7 @@
 require 'rails_helper'
 
 RSpec.describe Attendance, type: :model do
-  let!(:user) { User.create(name: 'Jhonathan', email: 'Jhon@test.com')}
+  let!(:user) { User.create(name: 'Jhonathan', email: 'Jhon@test.com') }
   let!(:event) { Event.create(title: 'A very nice and well tough tittle.', body: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum', location: 'Medellin', date: '2020-03-10', creator_id: user.id) }
   let!(:attendance) { Attendance.create(user_id: user.id, event_id: event.id) }
 
